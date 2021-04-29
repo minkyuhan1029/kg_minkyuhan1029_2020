332f34fabdf682fb8b611f5b452efb92ed2478e9
Min Kyu Han <minkyu.han1029@gmail.com>
2021-04-29 10:07:13 -0400

    Setting Up Implementation

---
+++ main.js
@@ -0,0 +1,14 @@
+const numbers = process.argv.slice(2)
+const phonetic = {
+  '0' : "Zero",
+  '1' : "One",
+  "2" : "Two",
+  "3" : "Three",
+  "4" : "Four",
+  "5" : "Five",
+  "6" : "Six",
+  "7" : "Seven",
+  "8" : "Eight",
+  "9" : "Nine"
+}
+
